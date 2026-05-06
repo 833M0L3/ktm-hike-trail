4b12b1fc3962a019c1f4cfb32c8360d517fe6d18
833M0L3 <bimaldhital@gmail.com>
2026-05-06 17:55:51 +0545

    fixed the elevation chart margin

--- src/components/ElevationChart.js
+++ src/components/ElevationChart.js
@@ -60,7 +60,7 @@ export default function ElevationChart({ route, color = '#f97316' }) {
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           data={data}
-          margin={{ top: 10, right: 10, left: -20, bottom: 0 }}
+          margin={{ top: 10, right: 10, left: 0, bottom: 0 }}
           onMouseMove={handleMouseMove}
           onMouseLeave={dispatchClear}
         >
